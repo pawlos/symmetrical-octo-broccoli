1a1ad36a9dd1b040abcfb1ececb6925a7439e718
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2024-03-03 20:45:07 +0100

    Initial profiler class w/ interface ICorProfilerCallback2

---
+++ src/profiler/profiler.cpp
@@ -0,0 +1,401 @@
+#include "profiler.h"
+
+HRESULT __stdcall MyProfiler::QueryInterface(REFIID riid, void** ppvObject)
+{
+	return E_NOTIMPL;
+}
+
+ULONG __stdcall MyProfiler::AddRef(void)
+{
+	return 0;
+}
+
+ULONG __stdcall MyProfiler::Release(void)
+{
+	return 0;
+}
+
+HRESULT __stdcall MyProfiler::Initialize(IUnknown* pICorProfilerInfoUnk)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::Shutdown(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::AppDomainCreationStarted(AppDomainID appDomainId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::AppDomainCreationFinished(AppDomainID appDomainId, HRESULT hrStatus)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::AppDomainShutdownStarted(AppDomainID appDomainId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::AppDomainShutdownFinished(AppDomainID appDomainId, HRESULT hrStatus)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::AssemblyLoadStarted(AssemblyID assemblyId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::AssemblyLoadFinished(AssemblyID assemblyId, HRESULT hrStatus)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::AssemblyUnloadStarted(AssemblyID assemblyId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::AssemblyUnloadFinished(AssemblyID assemblyId, HRESULT hrStatus)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ModuleLoadStarted(ModuleID moduleId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ModuleLoadFinished(ModuleID moduleId, HRESULT hrStatus)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ModuleUnloadStarted(ModuleID moduleId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ModuleUnloadFinished(ModuleID moduleId, HRESULT hrStatus)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ModuleAttachedToAssembly(ModuleID moduleId, AssemblyID AssemblyId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ClassLoadStarted(ClassID classId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ClassLoadFinished(ClassID classId, HRESULT hrStatus)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ClassUnloadStarted(ClassID classId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ClassUnloadFinished(ClassID classId, HRESULT hrStatus)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::FunctionUnloadStarted(FunctionID functionId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::JITCompilationStarted(FunctionID functionId, BOOL fIsSafeToBlock)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::JITCompilationFinished(FunctionID functionId, HRESULT hrStatus, BOOL fIsSafeToBlock)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::JITCachedFunctionSearchStarted(FunctionID functionId, BOOL* pbUseCachedFunction)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::JITCachedFunctionSearchFinished(FunctionID functionId, COR_PRF_JIT_CACHE result)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::JITFunctionPitched(FunctionID functionId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::JITInlining(FunctionID callerId, FunctionID calleeId, BOOL* pfShouldInline)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ThreadCreated(ThreadID threadId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ThreadDestroyed(ThreadID threadId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ThreadAssignedToOSThread(ThreadID managedThreadId, DWORD osThreadId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RemotingClientInvocationStarted(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RemotingClientSendingMessage(GUID* pCookie, BOOL fIsAsync)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RemotingClientReceivingReply(GUID* pCookie, BOOL fIsAsync)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RemotingClientInvocationFinished(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RemotingServerReceivingMessage(GUID* pCookie, BOOL fIsAsync)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RemotingServerInvocationStarted(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RemotingServerInvocationReturned(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RemotingServerSendingReply(GUID* pCookie, BOOL fIsAsync)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::UnmanagedToManagedTransition(FunctionID functionId, COR_PRF_TRANSITION_REASON reason)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ManagedToUnmanagedTransition(FunctionID functionId, COR_PRF_TRANSITION_REASON reason)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RuntimeSuspendStarted(COR_PRF_SUSPEND_REASON suspendReason)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RuntimeSuspendFinished(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RuntimeSuspendAborted(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RuntimeResumeStarted(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RuntimeResumeFinished(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RuntimeThreadSuspended(ThreadID threadId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RuntimeThreadResumed(ThreadID threadId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::MovedReferences(ULONG cMovedObjectIDRanges, ObjectID oldObjectIDRangeStart[], ObjectID newObjectIDRangeStart[], ULONG cObjectIDRangeLength[])
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ObjectAllocated(ObjectID objectId, ClassID classId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ObjectsAllocatedByClass(ULONG cClassCount, ClassID classIds[], ULONG cObjects[])
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ObjectReferences(ObjectID objectId, ClassID classId, ULONG cObjectRefs, ObjectID objectRefIds[])
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RootReferences(ULONG cRootRefs, ObjectID rootRefIds[])
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionThrown(ObjectID thrownObjectId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionSearchFunctionEnter(FunctionID functionId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionSearchFunctionLeave(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionSearchFilterEnter(FunctionID functionId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionSearchFilterLeave(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionSearchCatcherFound(FunctionID functionId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionOSHandlerEnter(UINT_PTR __unused)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionOSHandlerLeave(UINT_PTR __unused)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionUnwindFunctionEnter(FunctionID functionId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionUnwindFunctionLeave(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionUnwindFinallyEnter(FunctionID functionId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionUnwindFinallyLeave(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionCatcherEnter(FunctionID functionId, ObjectID objectId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionCatcherLeave(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::COMClassicVTableCreated(ClassID wrappedClassId, REFGUID implementedIID, void* pVTable, ULONG cSlots)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::COMClassicVTableDestroyed(ClassID wrappedClassId, REFGUID implementedIID, void* pVTable)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionCLRCatcherFound(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ExceptionCLRCatcherExecute(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::ThreadNameChanged(ThreadID threadId, ULONG cchName, WCHAR name[])
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::GarbageCollectionStarted(int cGenerations, BOOL generationCollected[], COR_PRF_GC_REASON reason)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::SurvivingReferences(ULONG cSurvivingObjectIDRanges, ObjectID objectIDRangeStart[], ULONG cObjectIDRangeLength[])
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::GarbageCollectionFinished(void)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::FinalizeableObjectQueued(DWORD finalizerFlags, ObjectID objectID)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::RootReferences2(ULONG cRootRefs, ObjectID rootRefIds[], COR_PRF_GC_ROOT_KIND rootKinds[], COR_PRF_GC_ROOT_FLAGS rootFlags[], UINT_PTR rootIds[])
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::HandleCreated(GCHandleID handleId, ObjectID initialObjectId)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT __stdcall MyProfiler::HandleDestroyed(GCHandleID handleId)
+{
+	return E_NOTIMPL;
+}
