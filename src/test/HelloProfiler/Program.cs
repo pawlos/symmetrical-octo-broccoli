8c045513cab19b06c0e08cec162984706209be51
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2024-03-11 19:56:32 +0100

    Test application added

---
+++ src/test/HelloProfiler/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Start");
+await Function1();
+Console.WriteLine("Done");
+return;
+
+
+// Few functions to have some stack traces
+async Task Function1()
+{
+    await Function2();
+}
+
+async Task Function2()
+{
+    await Function3();
+}
+
+async Task Function3()
+{
+    await Task.Delay(200);
+    Console.WriteLine("Hello, Profiler");
+}
