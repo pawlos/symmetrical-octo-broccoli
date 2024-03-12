1775b76c3e096bb3a0125ccb80714e81ae12a2a7
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2024-03-12 16:59:21 +0100

    More env variables set & fixes to the script

--- src/profiler/run.cmd
+++ src/profiler/run.cmd
@@ -1,7 +1,9 @@
+SET DOTNET_EnableDiagnostics=1
 SET COR_ENABLE_PROFILING=1
+SET CORECLR_ENABLE_PROFILING=1
 SET COR_PROFILER={DC27BF80-3A36-40D4-9278-6415508C4ED6}
+SET CORECLR_PROFILER={DC27BF80-3A36-40D4-9278-6415508C4ED6}
 SET COR_PROFILER_PATH=x64\Debug\OctoProfiler.dll
+SET CORECLR_PROFILER_PATH_64=x64\Debug\OctoProfiler.dll
 
-START ..\test\HelloProfiler\bin\Debug\net6.0\HelloProfiler.exe
-
-pause >nul
+START /wait /B ..\test\HelloProfiler\bin\Debug\net6.0\HelloProfiler.exe
