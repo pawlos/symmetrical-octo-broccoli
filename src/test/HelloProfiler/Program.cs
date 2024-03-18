7bcccc828aef29d5817da7a9aed397d3ccbc6a4c
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2024-03-18 19:06:17 +0100

    Better examples for profiler.

--- src/test/HelloProfiler/Program.cs
+++ src/test/HelloProfiler/Program.cs
@@ -1,13 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Waiting for a key");
+Console.WriteLine("Profile test program - start");
+WaitForAKey();
+
+Console.WriteLine("Profiler - GC collect test");
 GC.Collect();
-Console.ReadKey();
-Console.WriteLine("Start");
+WaitForAKey();
+
+Console.WriteLine("Profiler - Call stack test");
 await Function1();
-Console.WriteLine("Done");
+Console.WriteLine("Profile test program - done");
+WaitForAKey();
+
+Console.WriteLine("Profiler - Allocations test");
+var test = new int[256];
+var test2 = new Dictionary<string, int> { { "test", 12 } };
+Console.WriteLine("Print value {0} {1}", test[0], test2["test"]);
+
+WaitForAKey();
+Console.WriteLine("Profiler - Exceptions test");
+try
+{
+    throw new Exception("testing");
+}
+catch
+{
+    // swallow
+}
+
+WaitForAKey();
 return;
 
+void WaitForAKey()
+{
+    Console.WriteLine("Press [Enter]");
+    Console.ReadLine();
+    Console.WriteLine();
+    Console.WriteLine("-----------------------------");
+}
 
 // Few functions to have some stack traces
 async Task Function1()
