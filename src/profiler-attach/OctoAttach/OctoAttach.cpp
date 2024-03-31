9a4ac26d9fca51995c2bb1ad447c3225142f5571
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2024-03-31 09:55:03 +0200

    ATTACH: Code to prepare for the attachment to the process

--- src/profiler-attach/OctoAttach/OctoAttach.cpp
+++ src/profiler-attach/OctoAttach/OctoAttach.cpp
@@ -2,6 +2,10 @@
 //
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <metahost.h>
+#include <wchar.h>
 
 int main(int argc, char* argv[])
 {
@@ -9,8 +13,30 @@ int main(int argc, char* argv[])
     if (argc <= 1)
     {
         std::cout << "Invalid argument count\n";
+        return -1;
     }
 
+    auto pid = std::stoi(argv[1]);
+
+    std::cout << "PID: " << pid << "\n";
+    ICLRMetaHost* _metahost = nullptr;
+    HRESULT hr = CLRCreateInstance(CLSID_CLRMetaHost, IID_ICLRMetaHost, (LPVOID*)&_metahost);
+    if (FAILED(hr))
+    {
+        std::cout << "Could not obtain CLR Mata host.\n";
+        return -2;
+    }
+
+    ICLRRuntimeInfo* _runtimeinfo = nullptr;
+    hr = _metahost->GetRuntime(L"v4.0.30319", IID_ICLRRuntimeInfo, (LPVOID*)&_runtimeinfo);
+    if (FAILED(hr))
+    {
+        std::cout << "Could not obtain CLR Runtime info.\n";
+        return -3;
+    }
+
+    ICLRProfiling* _clrProfiling = nullptr;
+    hr = _runtimeinfo->GetInterface(CLSID_CLRProfiling, IID_ICLRProfiling, (LPVOID*)&_clrProfiling);
     return 0;
 
 }
