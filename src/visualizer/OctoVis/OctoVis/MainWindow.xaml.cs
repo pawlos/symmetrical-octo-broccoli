7a0ebf786ed5cad9d5406f5cc9d50465ee72019f
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2024-04-23 00:06:16 +0200

    FRONTEND: Starting the process with profiler set

--- src/visualizer/OctoVis/OctoVis/MainWindow.xaml.cs
+++ src/visualizer/OctoVis/OctoVis/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -96,14 +97,48 @@ public partial class MainWindow
 
     private void OpenLog_OnClick(object sender, RoutedEventArgs e)
     {
-        var ofd = new OpenFileDialog();
-        ofd.ShowDialog();
-        ParseFile(ofd.FileName);
+        ParseFile(PickFile("Open log file", "Log files|*.txt"));
     }
 
-    private void ProfileApp_OnClick(object sender, RoutedEventArgs e)
+    private string PickFile(string title, string allowedExtension)
     {
-        // TODO: implement
+        var ofd = new OpenFileDialog
+        {
+            Filter = allowedExtension,
+            Title = title
+        };
+        ofd.ShowDialog();
+        return ofd.FileName;
+    }
+
+    private async void ProfileApp_OnClick(object sender, RoutedEventArgs e)
+    {
+        var fileName = $"{Guid.NewGuid().ToString()}.txt";
+        var program = PickFile("Open application to profile", "Program|*.exe");
+        var p = new Process
+        {
+            StartInfo =
+            {
+                FileName = program,
+                EnvironmentVariables =
+                {
+                    ["DOTNET_EnableDiagnostics"] = "1",
+                    ["COR_ENABLE_PROFILING"] = "1",
+                    ["CORECLR_ENABLE_PROFILING"] = "1",
+                    ["COR_PROFILER"] = "{8A8CC829-CCF2-49FE-BBAE-0F022228071A}",
+                    ["CORECLR_PROFILER"] = "{8A8CC829-CCF2-49FE-BBAE-0F022228071A}",
+                    ["COR_PROFILER_PATH"] = @"..\..\..\..\..\..\profiler\x64\Debug\OctoProfiler.dll",
+                    ["CORECLR_PROFILER_PATH_64"] = @"..\..\..\..\..\..\profiler\x64\Debug\OctoProfiler.dll",
+                    ["OCTO_PROFILER_FILE"] = fileName
+                }
+            }
+        };
+        Hide();
+        p.Start();
+
+        await p.WaitForExitAsync();
+        Show();
+        ParseFile(fileName);
     }
 
     private void Control_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -112,7 +147,7 @@ public partial class MainWindow
 
         if (row.DataContext is not ProfilerViewModel.TypeAllocationsEntry tat) return;
 
-        StackTraceWindow stw = new StackTraceWindow
+        var stw = new StackTraceWindow
         {
             DataContext = tat
         };
