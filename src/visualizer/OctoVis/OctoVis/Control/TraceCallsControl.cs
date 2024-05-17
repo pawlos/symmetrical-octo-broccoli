83a003266faaa814163b0638ce625da837b60cae
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2024-05-17 05:38:11 +0200

    FRONTEND: New window/control to visualizer performance traces

---
+++ src/visualizer/OctoVis/OctoVis/Control/TraceCallsControl.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+using System.Windows.Media;
+
+namespace OctoVis.Control;
+
+public class TraceCallsControl : FrameworkElement
+{
+    protected override void OnRender(DrawingContext drawingContext)
+    {
+        base.OnRender(drawingContext);
+    }
+}
