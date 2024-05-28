ffea2b0f20077a06bc7240c66203fbbcdc6a4522
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2024-05-28 06:13:23 +0200

    FRONT: Drawing performance view

--- src/visualizer/OctoVis/OctoVis/Control/TraceCallsControl.cs
+++ src/visualizer/OctoVis/OctoVis/Control/TraceCallsControl.cs
@@ -23,23 +23,33 @@ public class TraceCallsControl : FrameworkElement
         base.OnRender(drawingContext);
         if (!EnterExitCalls?.Any() ?? true) return;
 
-        var longest = EnterExitCalls.MaxBy(x => x.Duration);
-        var brush = new SolidColorBrush(Colors.Brown);
-        drawingContext.DrawRectangle(brush, new Pen(Brushes.Tomato, 1.0),
-            new Rect(10, 10, ActualWidth-20, 30));
-        var ft = new FormattedText(
-            $"{longest.Class}.{longest.MethodName}",
-            CultureInfo.CurrentCulture,
-            FlowDirection.LeftToRight,
-            new Typeface(new FontFamily("Segoe UI"),
-                FontStyles.Normal,
-                FontWeights.Normal,
-                FontStretches.Normal),
-            20,
-            new SolidColorBrush(Colors.Black),
-            null,
-            TextFormattingMode.Display,
-            72);
-        drawingContext.DrawText(ft, new Point(15, 10));
+        var order = EnterExitCalls.OrderBy(x => x.StartTime).ToArray();
+
+        var width = ActualWidth;
+        var startX = 10.0d;
+        var totalDuration = order.Select(x => (int)x.Duration).Sum();
+
+        foreach (var item in order)
+        {
+            var itemWidth = (double)item.Duration / totalDuration * width;
+            var brush = new SolidColorBrush(Colors.Brown);
+            drawingContext.DrawRectangle(brush, new Pen(Brushes.Tomato, 1.0),
+                new Rect(startX, 10, itemWidth, 30));
+            var ft = new FormattedText(
+                $"{item.Class}.{item.MethodName}",
+                CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface(new FontFamily("Segoe UI"),
+                    FontStyles.Normal,
+                    FontWeights.Normal,
+                    FontStretches.Normal),
+                20,
+                new SolidColorBrush(Colors.Black),
+                null,
+                TextFormattingMode.Display,
+                72);
+            drawingContext.DrawText(ft, new Point(startX+5, 10));
+            startX += itemWidth;
+        }
     }
 }
