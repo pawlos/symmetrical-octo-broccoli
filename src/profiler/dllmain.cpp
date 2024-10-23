47ad9511133ff765d680c1cb679e457a4855db76
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2024-10-23 22:26:26 +0200

    Include annotations

--- src/profiler/dllmain.cpp
+++ src/profiler/dllmain.cpp
@@ -3,7 +3,7 @@
 
 static OctoProfilerFactory* factory;
 
-inline std::optional<std::string> get_env(const char* key)
+inline static std::optional<std::string> get_env(const char* key)
 {
 	if (key == nullptr) {
 		throw std::invalid_argument("Null pointer passed as environment variable name");
@@ -18,7 +18,7 @@ inline std::optional<std::string> get_env(const char* key)
 	return { std::string { buffer } };
 }
 
-inline bool to_bool(const std::string& str)
+inline static bool to_bool(const std::string& str)
 {
 	std::istringstream is(str);
 	bool b;
@@ -31,7 +31,8 @@ BOOL DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 	return TRUE;
 }
 
-extern "C" HRESULT STDMETHODCALLTYPE DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID *ppv)
+_Check_return_
+extern "C" HRESULT STDMETHODCALLTYPE DllGetClassObject(_In_ REFCLSID rclsid, _In_ REFIID riid, _Outptr_ LPVOID FAR* ppv)
 {
 	const auto file_to_log = get_env("OCTO_PROFILER_FILE");
 	const auto include_ts = to_bool(get_env("OCTO_LOGGER_INCLUDE_TS").value_or("true"));
